ed34ef7c39d885058094619e3cd215cf199c0d34
Tobias Nölle <tobias.noelle@icloud.com>
2024-11-17 14:19:51 +0100

    added input fields for name, birthday and the address

--- src/app/user/dialog-add-user/dialog-add-user.component.ts
+++ src/app/user/dialog-add-user/dialog-add-user.component.ts
@@ -1,14 +1,24 @@
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialogModule } from '@angular/material/dialog';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import { provideNativeDateAdapter } from '@angular/material/core';
+import { MatIconModule } from '@angular/material/icon';
 
 
 @Component({
   selector: 'app-dialog-add-user',
   standalone: true,
+  providers: [provideNativeDateAdapter()],
   imports: [
     MatDialogModule,
-    MatButtonModule
+    MatButtonModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatDatepickerModule,
+    MatIconModule
   ],
   templateUrl: './dialog-add-user.component.html',
   styleUrl: './dialog-add-user.component.scss'
