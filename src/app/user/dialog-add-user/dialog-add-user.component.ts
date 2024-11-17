f98abd3482475da3735ce1b94b0ca4dd4aec0635
Tobias Nölle <tobias.noelle@icloud.com>
2024-11-17 17:26:30 +0100

    added ngModel binding with a user variable and added saveUser() function

--- src/app/user/dialog-add-user/dialog-add-user.component.ts
+++ src/app/user/dialog-add-user/dialog-add-user.component.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialogModule } from '@angular/material/dialog';
-import {MatInputModule} from '@angular/material/input';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatDatepickerModule} from '@angular/material/datepicker';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatDatepickerModule } from '@angular/material/datepicker';
 import { provideNativeDateAdapter } from '@angular/material/core';
 import { MatIconModule } from '@angular/material/icon';
+import { User } from '../../shared/models/user.class';
+import { FormsModule } from '@angular/forms';
 
 
 @Component({
@@ -18,11 +20,24 @@ import { MatIconModule } from '@angular/material/icon';
     MatInputModule,
     MatFormFieldModule,
     MatDatepickerModule,
-    MatIconModule
+    MatIconModule,
+    FormsModule
   ],
   templateUrl: './dialog-add-user.component.html',
   styleUrl: './dialog-add-user.component.scss'
 })
 export class DialogAddUserComponent {
+  user = new User();
+  birthDate: any;
+
+  constructor() {
+    
+  }
+
+  saveUser() {
+    this.user.birthDate = this.birthDate.getTime();
+    console.log('user :>> ', this.user);
+    console.log('birthDate :>> ', this.birthDate);
+  }
 
 }
