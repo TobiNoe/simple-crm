89ccd15ba68b40d8f807123c523b1d6878739354
Tobias Nölle <tobias.noelle@icloud.com>
2024-11-19 19:14:20 +0100

    changed function saveUser() to save testdata into firestore database

--- src/app/user/dialog-add-user/dialog-add-user.component.ts
+++ src/app/user/dialog-add-user/dialog-add-user.component.ts
@@ -1,4 +1,4 @@
-import { Component, NgModule } from '@angular/core';
+import { Component, inject, NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatInputModule } from '@angular/material/input';
@@ -8,6 +8,7 @@ import { provideNativeDateAdapter } from '@angular/material/core';
 import { MatIconModule } from '@angular/material/icon';
 import { User } from '../../shared/models/user.class';
 import { FormsModule } from '@angular/forms';
+import { addDoc, collection, Firestore } from '@angular/fire/firestore';
 
 
 @Component({
@@ -27,17 +28,32 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './dialog-add-user.component.scss'
 })
 export class DialogAddUserComponent {
+  firestore: Firestore = inject(Firestore);
   user = new User();
   birthDate: any;
 
   constructor() {
-    
+
   }
 
-  saveUser() {
+  async saveUser() {
     this.user.birthDate = this.birthDate.getTime();
     console.log('user :>> ', this.user);
     console.log('birthDate :>> ', this.birthDate);
+
+    // Add a new document with a generated id.
+    const docRef = await addDoc(collection(this.firestore, "users"), {
+      lastName: this.user.lastName,
+      firstName: this.user.firstName
+    });
+    console.log("Document written with ID: ", docRef.id);
+
+    /*  this.firestore
+     .collection('users')
+     .add(this.user)
+     .then((result: any){
+       console.log('Adding User finished:>> ', result );
+     }); */
   }
 
 }
