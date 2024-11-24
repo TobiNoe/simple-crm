8e80bd35d14742c6f2c217eab6b4e52b29d75d6a
Tobias Nölle <tobias.noelle@icloud.com>
2024-11-24 14:18:25 +0100

    FirestoreService: added function getCollectionRef(), getDocRef(), subscribeCollection() and ngOnDestroy()

--- src/app/shared/services/firestore.service.ts
+++ src/app/shared/services/firestore.service.ts
@@ -1,26 +1,43 @@
 import { inject, Injectable } from '@angular/core';
-import { addDoc, collection, doc, Firestore } from '@angular/fire/firestore';
+import { addDoc, collection, doc, Firestore, onSnapshot } from '@angular/fire/firestore';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FirestoreService {
   private firestore: Firestore = inject(Firestore);
+  users = [];
+  unsubUsers;
 
   constructor() {
-    this.getRef();
+    console.log('DocRef :>> ', this.getDocRef("users", "2FjsaVtxH1pxsbEjH1TT"));
+    this.unsubUsers = this.subscribeCollection("users");
   }
 
-  getRef() {
-    const usersRef = collection(this.firestore, "users");
-    console.log('usersRef :>> ', usersRef);
-    const userRef = doc(this.firestore, "users", "2FjsaVtxH1pxsbEjH1TT");
-    console.log('userRef :>> ', userRef);
+  getCollectionRef(CollectionID: string) {
+    return collection(this.firestore, CollectionID);
   }
 
-  /* ngOnDestroy() {
+  getDocRef(collectionID: string, docID: string) {
+    return doc(this.getCollectionRef(collectionID), docID);
+  }
 
-  } */
+  subscribeCollection(CollectionID: string) {
+    return onSnapshot(this.getCollectionRef(CollectionID), (list) => {
+      this.users = [];
+      list.forEach(element => {
+        console.log('element.data() :>> ', element.data());
+        console.log('element.id :>> ', element.id);
+        /* this.users.push(this.setNoteObject(element.data(), element.id)); */
+      });
+    });
+  }
+
+
+
+  ngOnDestroy() {
+    this.unsubUsers();
+  }
 
   async saveUserInDB(user: any) {
     const docRef = await addDoc(collection(this.firestore, "users"), user)
