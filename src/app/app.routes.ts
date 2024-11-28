bc8804b6726b383108ba58925d0395a6871348e8
Tobias Nölle <tobias.noelle@icloud.com>
2024-11-28 17:21:43 +0100

    created UserDetailComponent and added route to user-detai-/:id

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { UserComponent } from './user/user.component';
+import { UserDetailComponent } from './user/user-detail/user-detail.component';
 
 export const routes: Routes = [
     {
@@ -10,6 +11,10 @@ export const routes: Routes = [
     {
         path: 'user',
         component: UserComponent,
+    },
+    {
+        path: 'user-detail/:id',
+        component: UserDetailComponent,
     },
     {
         path: '',
