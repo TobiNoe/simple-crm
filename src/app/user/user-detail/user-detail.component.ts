0d5155eb431ac5d2aa8b41b9ce67d58a8819112d
Tobias Nölle <tobias.noelle@icloud.com>
2024-11-28 18:39:18 +0100

    UserDetailComponent ts: added function ngOnInit() to get userID from activatedRoute

--- src/app/user/user-detail/user-detail.component.ts
+++ src/app/user/user-detail/user-detail.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-user-detail',
   standalone: true,
-  imports: [],
+  imports: [
+    MatCardModule
+  ],
   templateUrl: './user-detail.component.html',
   styleUrl: './user-detail.component.scss'
 })
-export class UserDetailComponent {
+export class UserDetailComponent implements OnInit{
+  user: any;
+  
+  constructor(private route: ActivatedRoute) {}
+
+  async ngOnInit() {
+    const userId = this.route.snapshot.paramMap.get('id'); // ID aus der Route
+    console.log('UserID :>> ', userId);
+  }
 
 }
