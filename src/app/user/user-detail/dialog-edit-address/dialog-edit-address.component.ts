8c792d7d20824670e970160bd3b2309311b20db5
Tobias Nölle <tobias.noelle@icloud.com>
2024-11-29 17:09:04 +0100

    dialogEditAddressComponent: ts html added inputfields for address and import all moduls

--- src/app/user/user-detail/dialog-edit-address/dialog-edit-address.component.ts
+++ src/app/user/user-detail/dialog-edit-address/dialog-edit-address.component.ts
@@ -1,12 +1,38 @@
 import { Component } from '@angular/core';
+import { User } from '../../../shared/models/user.class';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule, MatDialogRef } from '@angular/material/dialog';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatIconModule } from '@angular/material/icon';
+import { FormsModule } from '@angular/forms';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-dialog-edit-address',
   standalone: true,
-  imports: [],
+  imports: [
+    MatDialogModule,
+    MatButtonModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatDatepickerModule,
+    MatIconModule,
+    FormsModule,
+    MatProgressBarModule,
+    NgIf
+  ],
   templateUrl: './dialog-edit-address.component.html',
   styleUrl: './dialog-edit-address.component.scss'
 })
 export class DialogEditAddressComponent {
+  user: User = new User;
+  loading = false;
+
+  saveUser() {
+
+  }
 
 }
