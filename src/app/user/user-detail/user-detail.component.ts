53e85bed57230dd900e547ddcd3aa95d88a99b30
Tobias Nölle <tobias.noelle@icloud.com>
2024-11-29 16:26:55 +0100

    UserDetailComponent ts html scss: added menu + button in header and address card to open menu

--- src/app/user/user-detail/user-detail.component.ts
+++ src/app/user/user-detail/user-detail.component.ts
@@ -4,7 +4,8 @@ import { ActivatedRoute } from '@angular/router';
 import { FirestoreService } from '../../shared/services/firestore.service';
 import { User } from '../../shared/models/user.class';
 import { MatIcon } from '@angular/material/icon';
-import { MatButton, MatButtonModule } from '@angular/material/button';
+import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
 
 @Component({
   selector: 'app-user-detail',
@@ -12,24 +13,28 @@ import { MatButton, MatButtonModule } from '@angular/material/button';
   imports: [
     MatCardModule,
     MatIcon,
-    MatButtonModule
+    MatButtonModule,
+    MatMenuModule
   ],
   templateUrl: './user-detail.component.html',
   styleUrl: './user-detail.component.scss'
 })
-export class UserDetailComponent implements OnInit{
+export class UserDetailComponent implements OnInit {
   firestoreService = inject(FirestoreService);
   user: User = new User();
-  
-  constructor(private route: ActivatedRoute) {}
+
+  constructor(private route: ActivatedRoute) { }
 
   async ngOnInit() {
     const userId = this.route.snapshot.paramMap.get('id');
     this.user = new User(await this.firestoreService.getUser(userId));
   }
 
-  openAddressDialog() {
-    console.log('Dialog :>> openAdressDialog()');
+  editUser() {
+    console.log('Dialog :>> editUser()');
   }
 
+  editAddress() {
+    console.log('Dialog :>> editAddress()');
+  }
 }
