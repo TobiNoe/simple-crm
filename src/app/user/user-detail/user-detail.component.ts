0ec6dbf99c122f0c80b2ba23077a978664effb64
Tobias Nölle <tobias.noelle@icloud.com>
2024-11-29 15:57:05 +0100

    UserDetailComponent ts html scss: added button to open function openAddressDialog()

--- src/app/user/user-detail/user-detail.component.ts
+++ src/app/user/user-detail/user-detail.component.ts
@@ -3,12 +3,16 @@ import { MatCardModule } from '@angular/material/card';
 import { ActivatedRoute } from '@angular/router';
 import { FirestoreService } from '../../shared/services/firestore.service';
 import { User } from '../../shared/models/user.class';
+import { MatIcon } from '@angular/material/icon';
+import { MatButton, MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-user-detail',
   standalone: true,
   imports: [
-    MatCardModule
+    MatCardModule,
+    MatIcon,
+    MatButtonModule
   ],
   templateUrl: './user-detail.component.html',
   styleUrl: './user-detail.component.scss'
@@ -22,6 +26,10 @@ export class UserDetailComponent implements OnInit{
   async ngOnInit() {
     const userId = this.route.snapshot.paramMap.get('id');
     this.user = new User(await this.firestoreService.getUser(userId));
+  }
+
+  openAddressDialog() {
+    console.log('Dialog :>> openAdressDialog()');
   }
 
 }
